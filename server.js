0aa6071dc7e0ae3ecb6423e270a47dbb810edeb1
nazril <nzrlfuad@gmail.com>
2026-09-08 17:13:49 +0700

    test minimal serverless endpoint

--- server.js
+++ server.js
@@ -1,34 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
-
-const sequelize = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
-const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Backend CampusHub jalan");
+  res.send("Backend CampusHub Berhasil Jalan!");
 });
-
-// Koneksi DB secara asynchronous tanpa memblokir serverless function
-sequelize
-  .authenticate()
-  .then(() => console.log("Database connected to Aiven"))
-  .catch((err) => console.error("Database connection error:", err));
-
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
 
 module.exports = app;
